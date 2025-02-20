79f6415daa780c94e7d2153ce000eaabb80e079a
NVRVV <varmanamepalli003@gmail.com>
2025-02-20 16:41:05 +0530

    backend

--- backend/api/api.js
+++ backend/api/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const dataController = require('../controllers/dataController');
+const dataController = require('./api/controllers/dataController');
 
 router.get('/details', dataController.getEducationData);
 
